a16ee9d09d8900be322c349f533d87026f19ac65
Soumya D. Mohanty <soumya.mohanty@utrgv.edu>
2025-11-25 11:33:35 -0600

    SDM: Corrected bug in statgaussnoisegen. There was an extra output argument.wq

--- NOISE/statgaussnoisegen.m
+++ NOISE/statgaussnoisegen.m
@@ -1,4 +1,5 @@
-function [inNoise,outNoise] = statgaussnoisegen(nSamples,psdVals,fltrOrdr,sampFreq)
+%function [inNoise,outNoise] = statgaussnoisegen(nSamples,psdVals,fltrOrdr,sampFreq)
+function outNoise = statgaussnoisegen(nSamples,psdVals,fltrOrdr,sampFreq)
 %Generate a realization of stationary Gaussian noise with given 2-sided PSD
 %Y = STATGAUSSNOISEGEN(N,PSD,O,Fs)
 %Generates a realization Y of stationary gaussian noise with a target
